28751c4196887bf2dac266d545a896b697170e2c
RainBowT <rainbowt0506@gmail.com>
2024-02-05 00:12:26 +0800

    feat: Dagger-Hilt Setup
    Implemented Dagger Hilt for dependency injection.
    
    - Created NoteApp class annotated with @HiltAndroidApp for Dagger Hilt setup.
    - Defined AppModule with dependency provides for NoteDatabase, NoteRepository, and NoteUseCases.
    - Configured AppModule to install dependencies in SingletonComponent.
    - Enhanced modularity and maintainability by centralizing dependency injection logic.

---
+++ app/src/main/java/com/rainbowt/philipplackner_cleanarchitecturenoteapp/NoteApp.kt
@@ -0,0 +1,7 @@
+package com.rainbowt.philipplackner_cleanarchitecturenoteapp
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class NoteApp : Application()
