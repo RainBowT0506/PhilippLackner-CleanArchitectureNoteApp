149199f82b4b17ffb320595bf57ce3fe48f0291c
RainBowT <rainbowt0506@gmail.com>
2024-02-04 23:28:05 +0800

    feat: GetNotes use case
    Implemented GetNotes use case for retrieving and sorting notes.
    
    Created GetNotes class as a use case for fetching and ordering notes.
    Utilized sealed classes NoteOrder and OrderType for defining sorting criteria.
    Applied dependency injection with NoteRepository in the use case.
    Implemented business logic to sort notes based on specified criteria.
    Enhanced code readability and reusability with the use of sealed classes.

---
+++ app/src/main/java/com/rainbowt/philipplackner_cleanarchitecturenoteapp/feature_note/domain/util/OrderType.kt
@@ -0,0 +1,6 @@
+package com.rainbowt.philipplackner_cleanarchitecturenoteapp.feature_note.domain.util
+
+sealed class OrderType {
+    object Ascending : OrderType()
+    object Descending : OrderType()
+}
